27b62cc116e48c32cbf8e68fb0c783d192179174
“neomdavid” <neomanaogdavid@gmail.com>
2024-07-21 23:46:29 +0800

    Initialize repository -- working project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <main>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </main>
+  );
+}
+
+export default App;
